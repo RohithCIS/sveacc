0414590f0edb7994bf0cffc2b3ae4caae8d52ca5
Rohith <rohith.mudaliar@gmail.com>
2017-07-31 19:35:18 +0530

    Modular Coded

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
     include realpath($_SERVER["DOCUMENT_ROOT"]).'/functions/config.php';
-    
+    if (isset($_GET['error_code'])) {
+        $errorm=$_GET['error_code'];
+    }
+    else {
+        $errorm="";
+    }
 ?>
 <html>
 <head>
@@ -14,9 +19,10 @@
     </div>
     <div class="col-md-4 col-sm-10 col-xs-10 contl">
         <img src="/assets/svelogo.png">
-        <form class="logf" method="POST" action="/pages/connect.php">
+        <form class="logf" method="POST" action="/functions/login.php">
             <input type="text" placeholder="Username" name="name" required><br>
             <input type="password" placeholder="Password" name="pwd" required><br>
+            <?php echo $errorm; ?><br>
             <button type="submit">Login</button>
         </form>
     </div>
